dfbac5f7127676cc4672d3053b946a1ab6c07f09
lauraq5 <lauraquintana@colegioguanenta.edu.co>
2023-03-09 14:56:33 -0500

    Carpetas de los ejercicios

--- ejercicio 1/ejercicio1.py
+++ ejercicio 1/ejercicio1.py
@@ -9,11 +9,7 @@ if x==0:
     else:
         print("La coordenada esta en el eje y.")
 else:
-    if y==0:
-        print("La coordenada esta en el eje x.")
-    else:
-        if x>0:
-            if y>0:
+    if y==0: vbgwqegf
                 print("La coordenada esta en el cuadrante 1")
             else:
                 print("La coordenada esta en el cuadrante 4.")
